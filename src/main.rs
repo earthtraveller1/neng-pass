5504c2c27d989836973244b28ed4f6e6a4c3c4e1
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-01-29 13:23:11 -0500

    Abstracted querying the master key into its own function.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{fs::File, io::{Write, Read}, path::Path};
+use std::{
+    fs::File,
+    io::{Read, Write},
+    path::Path,
+};
 
 use clap::{arg, Command};
 use directories::ProjectDirs;
@@ -14,6 +18,35 @@ fn cli() -> Command {
                 .about("Sets the master key")
                 .arg(arg!(<NEW_KEY> "the master key you want to set it to")),
         )
+}
+
+fn query_master_key(p_message: &str, p_master_key_file: &mut File) -> Option<String> {
+    eprint!("{}", p_message);
+
+    let mut user_input_key = String::new();
+    std::io::stdin()
+        .read_line(&mut user_input_key)
+        .expect("Failed to read the input string.");
+    let user_input_key = user_input_key.trim();
+
+    let user_input_key_hashed = Sha512::digest(user_input_key);
+    let mut actual_key_hashed = Vec::new();
+    if p_master_key_file
+        .read_to_end(&mut actual_key_hashed)
+        .is_err()
+    {
+        eprintln!(
+            "[ERROR]: Failed to read the master key. Maybe you don't have permission to read it?"
+        );
+        return None;
+    }
+
+    if user_input_key_hashed.as_slice() != actual_key_hashed.as_slice() {
+        eprintln!("[ERROR]: That is the wrong master key.");
+        return None;
+    }
+
+    Some(user_input_key.to_string())
 }
 
 fn main() {
@@ -38,18 +71,7 @@ fn main() {
         Some(("set-master", sub_matches)) => {
             let master_key_file = File::open(format!("{}/master_key", data_dir));
             if let Ok(mut master_key_file) = master_key_file {
-                eprint!("Please enter the old master key: ");
-
-                let mut user_input_key = String::new();
-                std::io::stdin().read_line(&mut user_input_key).expect("Failed to read the input string.");
-                let user_input_key = user_input_key.trim();
-
-                let user_input_key_hashed = Sha512::digest(user_input_key);
-                let mut actual_key_hashed = Vec::new();
-                master_key_file.read_to_end(&mut actual_key_hashed).expect("Failed to read the master key file.");
-
-                if user_input_key_hashed.as_slice() != actual_key_hashed.as_slice() {
-                    eprintln!("That is incorrect.");
+                if query_master_key("Enter the old master key: ", &mut master_key_file).is_none() {
                     std::process::exit(1);
                 }
             }
