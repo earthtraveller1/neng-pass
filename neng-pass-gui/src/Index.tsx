24fead88b7c30cff8cc75657bd383052cff39ddc
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-02-01 18:23:54 -0500

    Added a basic title element to the UI.

--- neng-pass-gui/src/Index.tsx
+++ neng-pass-gui/src/Index.tsx
@@ -1,3 +1,5 @@
 export default function Index() {
-    return <h1>Hello, World!</h1>
+    return <>
+        <h1 class="text-4xl text-left mt-10 ml-10 font-bold bg-neutral-950 w-fit p-5 rounded-xl">Your Passwords</h1>
+    </>
 }
