0e429b13124a07bbcd7508a77826ac71c50716c7
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-01-31 20:37:32 -0500

    Added functions for encrypting and decrypting stuff.

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,6 +1,9 @@
+use aes::cipher::{generic_array::GenericArray, KeyInit, BlockEncrypt, BlockDecrypt};
 use argon2::{password_hash::SaltString, Argon2, PasswordHasher};
 use rand::SeedableRng;
 use rand_chacha::ChaCha20Rng;
+
+use crate::{MAX_PASSWORD_LEN, MAX_MASTER_KEY_LEN};
 
 pub fn hash(p_input: &str) -> argon2::password_hash::Result<Vec<u8>> {
     let mut cha_cha_rng = ChaCha20Rng::from_entropy();
@@ -11,4 +14,36 @@ pub fn hash(p_input: &str) -> argon2::password_hash::Result<Vec<u8>> {
         .to_string()
         .as_bytes()
         .to_owned())
+}
+
+pub fn encrypt(p_key: &[u8], p_data: &[u8]) -> [u8; MAX_PASSWORD_LEN] {
+    let mut key = [0u8; MAX_MASTER_KEY_LEN];
+    key.copy_from_slice(p_key);
+    let key_block = GenericArray::from(key);
+
+    let mut data = [0u8; MAX_PASSWORD_LEN];
+    data.copy_from_slice(p_data);
+    let mut data_block = GenericArray::from(data);
+
+    let cipher = aes::Aes256::new(&key_block);
+    cipher.encrypt_block(&mut data_block);
+    data.copy_from_slice(data_block.as_slice());
+
+    data
+}
+
+pub fn decrypt(p_key: &[u8], p_data: &[u8]) -> [u8; MAX_PASSWORD_LEN] {
+    let mut key = [0u8; MAX_MASTER_KEY_LEN];
+    key.copy_from_slice(p_key);
+    let key_block = GenericArray::from(key);
+
+    let mut data = [0u8; MAX_PASSWORD_LEN];
+    data.copy_from_slice(p_data);
+    let mut data_block = GenericArray::from(data);
+
+    let cipher = aes::Aes256::new(&key_block);
+    cipher.decrypt_block(&mut data_block);
+    data.copy_from_slice(data_block.as_slice());
+
+    data
 }
