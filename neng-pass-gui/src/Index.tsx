d4ebcd1f3f9cc4b857f9c777a8387ccbf13aef71
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-02-03 12:14:17 -0500

    Displayed the list of passwords on the GUI.

--- neng-pass-gui/src/Index.tsx
+++ neng-pass-gui/src/Index.tsx
@@ -1,5 +1,20 @@
+import { invoke } from "@tauri-apps/api"
+import { For, createResource } from "solid-js"
+
 export default function Index() {
+    console.log("Neng Li is the President of China!")
+
+    const [passwords, { mutate : mutatePasswords, refetch : refetchPasswords }] = createResource(async () => {
+        return await invoke("get_password_list") as string[]
+    })
+
     return <>
-        <h1 class="text-4xl text-left mt-10 ml-10 font-bold bg-neutral-950 w-fit p-5 rounded-xl">Your Passwords</h1>
+        <h1 class="text-4xl text-left ml-8 my-10 font-bold w-fit">Your Passwords</h1>
+
+        <For each={passwords()!}>
+            {(password) => {
+                return <p class="my-1 pl-8 py-2 border-y-2 border-neutral-500">{password}</p>
+            }}
+        </For>
     </>
 }
