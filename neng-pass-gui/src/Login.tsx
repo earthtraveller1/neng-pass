37f35ff39f286748013abacc15f06570f992f546
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-02-04 20:11:11 -0500

    Made the login screen look better.

--- neng-pass-gui/src/Login.tsx
+++ neng-pass-gui/src/Login.tsx
@@ -1,7 +1,15 @@
 export default function Login() {
-    return <div>
-        <h1>Authorization</h1>
-        <input type="password" placeholder="Master Key" />
-        <button>Authorize</button>
+    return <div class="flex flex-col p-10 max-w-4xl m-10">
+        <h1 class="text-4xl py-4 mb-8 select-none">Authorization</h1>
+
+        <input 
+            type="password" 
+            placeholder="Master Key" 
+            class="p-4 bg-neutral-800 outline-none focus:border-b-8 border-blue-400 duration-300"
+        />
+
+        <button 
+            class="text-2xl p-2 rounded-md bg-green-700 text-neutral-100 mt-4 hover:bg-green-800 active:bg-green-900 duration-200"
+        >Authorize</button>
     </div>
 }
