e1ffc694156ea69cef0291a57e4ffc3a4be1f839
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-02-04 19:57:32 -0500

    Added a login page.

--- neng-pass-gui/src/Index.tsx
+++ neng-pass-gui/src/Index.tsx
@@ -1,5 +1,7 @@
 import { invoke } from "@tauri-apps/api"
-import { For, createResource } from "solid-js"
+import { For, Show, createResource, createSignal } from "solid-js"
+import { Page } from "./common"
+import Login from "./Login"
 
 export default function Index() {
     console.log("Neng Li is the President of China!")
@@ -8,21 +10,28 @@ export default function Index() {
         return await invoke("get_password_list") as string[]
     })
 
-    return <>
-        <div class="flex flex-row">
-            <h1 class="text-4xl text-left ml-8 my-10 font-bold w-fit">Your Passwords</h1>
-            <button 
-                class={
-                    "text-4xl text-right w-fit bg-green-600 px-4 rounded-xl " + 
-                    "my-8 ml-auto mr-10 hover:bg-green-700 active:bg-green-800 duration-150"
-                }
-            >+</button>
-        </div>
+    const [getCurrentPage, setCurrentPage] = createSignal(Page.Login)
 
-        <For each={passwords()!}>
-            {(password) => {
-                return <p class="my-1 pl-8 py-2 border-y-2 border-neutral-500">{password}</p>
-            }}
-        </For>
+    return <>
+        <Show when={getCurrentPage() == Page.Login}>
+            <Login />
+        </Show>
+        <Show when={getCurrentPage() == Page.Passwords}>
+            <div class="flex flex-row">
+                <h1 class="text-4xl text-left ml-8 my-10 font-bold w-fit">Your Passwords</h1>
+                <button 
+                    class={
+                        "text-4xl text-right w-fit bg-green-600 px-4 rounded-xl " + 
+                        "my-8 ml-auto mr-10 hover:bg-green-700 active:bg-green-800 duration-150"
+                    }
+                >+</button>
+            </div>
+
+            <For each={passwords()!}>
+                {(password) => {
+                    return <p class="my-1 pl-8 py-2 border-y-2 border-neutral-500">{password}</p>
+                }}
+            </For>
+        </Show>
     </>
 }
---
+++ neng-pass-gui/src/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+    return <div>
+        <h1>Authorization</h1>
+        <input type="password" placeholder="Master Key" />
+        <button>Authorize</button>
+    </div>
+}
