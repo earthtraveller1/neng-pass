fa56ee74011181c2995ee00fa02707aea621d1fa
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-02-17 17:15:49 -0500

    Added a bit of margin below the password input boxes.

--- neng-pass-gui/src/PasswordInput.tsx
+++ neng-pass-gui/src/PasswordInput.tsx
@@ -9,7 +9,7 @@ export default function PasswordInput(props: InputProps) {
     return <input 
         type="password" 
         placeholder={props.label}
-        class="p-4 bg-neutral-800 outline-none focus:border-b-8 border-blue-400 duration-300 hover:bg-neutral-700 focus:bg-slate-800"
+        class="p-4 bg-neutral-800 mb-4 outline-none focus:border-b-8 border-blue-400 duration-300 hover:bg-neutral-700 focus:bg-slate-800"
         onInput={props.onInput}
     />
 }
