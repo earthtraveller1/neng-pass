8cb53658036a5f8ecffa846a17a5466df2899441
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-02-19 17:56:43 -0500

    Shows the Setup page when the master key is not set.

--- neng-pass-gui/src/Index.tsx
+++ neng-pass-gui/src/Index.tsx
@@ -1,7 +1,9 @@
-import { Show, createContext, createSignal } from "solid-js"
+import { Show, createContext, createResource, createSignal } from "solid-js"
 import { Page } from "./common"
 import Login from "./Login"
+import Setup from "./Setup"
 import Passwords from "./Passwords"
+import { invoke } from "@tauri-apps/api"
 
 export interface PageContextType {
     setPage: (newPage: Page) => void,
@@ -15,10 +17,17 @@ export default function Index() {
 
     const [getCurrentPage, setCurrentPage] = createSignal(Page.Login)
 
+    const [isMasterKeySet, _] = createResource(async () => {
+        const isMasterKeySet = await invoke<boolean>("is_master_key_set")
+        return isMasterKeySet
+    })
+
     return <>
         <Show when={getCurrentPage() == Page.Login}>
             <PageContext.Provider value={{setPage: setCurrentPage, getPage: getCurrentPage }}>
-                <Login />
+                <Show when={isMasterKeySet()} fallback={<Setup />}> 
+                    <Login />
+                </Show>
             </PageContext.Provider>
         </Show>
         <Show when={getCurrentPage() == Page.Passwords}>
