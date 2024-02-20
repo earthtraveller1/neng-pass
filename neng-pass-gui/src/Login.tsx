0efdc7a9364d8695469f03cda86e44e260b3f12c
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-02-19 19:00:15 -0500

    Abstracted the error box into a component of it's own.

--- neng-pass-gui/src/Login.tsx
+++ neng-pass-gui/src/Login.tsx
@@ -4,6 +4,7 @@ import { Page } from "./common"
 import { invoke } from "@tauri-apps/api"
 import PasswordInput from "./components/PasswordInput"
 import Button from "./components/Button"
+import ErrorBox from "./components/ErrorBox"
 
 export default function Login() {
     const pageContext = useContext(PageContext)
@@ -17,9 +18,9 @@ export default function Login() {
     return <div class="flex flex-col p-10 max-w-4xl m-10">
         <h1 class="text-4xl py-4 mb-8 select-none">Authorization</h1>
 
-        <div class={`text-xl bg-red-800 ${getMessageHeight()} duration-500 rounded-xl overflow-hidden text-center select-none`}>
+        <ErrorBox heightClass={getMessageHeight()}>
             Incorrect Master Key
-        </div>
+        </ErrorBox>
 
         <PasswordInput label="Master Key" onInput={event => {
             setEnteredPassword(event.target.value)
---
+++ neng-pass-gui/src/components/ErrorBox.tsx
@@ -0,0 +1,12 @@
+import { JSX } from "solid-js/jsx-runtime"
+
+interface ErrorBoxProps {
+    heightClass: string,
+    children: JSX.Element
+}
+
+export default function ErrorBox(props: ErrorBoxProps) {
+    return <div class={`text-xl bg-red-800 ${props.heightClass} duration-500 rounded-xl overflow-hidden text-center select-none`}>
+        ${props.children}
+    </div>
+}
