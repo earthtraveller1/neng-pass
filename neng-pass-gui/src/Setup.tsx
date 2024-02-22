e9335bf153c33f9c0349159bcc8386302d196b71
earthtraveller1 <liangyuezhao7@gmail.com>
2024-02-21 19:14:20 -0500

    Made the UI actually do something.

--- neng-pass-gui/src/Setup.tsx
+++ neng-pass-gui/src/Setup.tsx
@@ -1,14 +1,36 @@
 import PasswordInput from "./components/PasswordInput"
 import Button from "./components/Button"
+import { createSignal } from "solid-js"
+import ErrorBox from "./components/ErrorBox"
+import { invoke } from "@tauri-apps/api"
 
 export default function Setup() {
+    const [getNewMasterKey, setNewMasterKey] = createSignal("")
+    const [getConfirmMasterKey, setConfirmMasterKey] = createSignal("")
+    const [getErrorBoxHeightClass, setErrorBoxHeightClass] = createSignal("max-h-0")
+
     return <div class="flex flex-col p-2 max-w-4xl m-10">
         <h1 class="text-4xl pb-4 select-none">Setup</h1>
         <p class="mb-8">Please set your master key to use.</p>
 
-        <PasswordInput label="New Master Key" onInput={() => {}} />
-        <PasswordInput label="Confirm Master Key" onInput={() => {}} />
+        <ErrorBox heightClass={getErrorBoxHeightClass()}>The keys that you entered do not match</ErrorBox>
 
-        <Button label="Continue" onClick={undefined} />
+        <PasswordInput label="New Master Key" onInput={(event) => {
+            setErrorBoxHeightClass("max-h-0")
+            setNewMasterKey(event.target.value.trim())
+        }} />
+        <PasswordInput label="Confirm Master Key" onInput={(event) => {
+            setErrorBoxHeightClass("max-h-0")
+            setConfirmMasterKey(event.target.value.trim())
+        }} />
+
+        <Button label="Continue" onClick={() => {
+            if (getNewMasterKey() != getConfirmMasterKey()) {
+                setErrorBoxHeightClass("max-h-auto p-2 mb-6")
+            } else {
+                invoke("set_new_master_key", { pNewMasterKey: getNewMasterKey() })
+                invoke("set_master_key", { pMasterKey: getNewMasterKey() })
+            }
+        }} />
     </div>
 }
