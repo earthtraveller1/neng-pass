983e2b3b4049cb657799ebe31fd9a8b71f357687
earthtraveller1 <liangyuezhao7@gmail.com>
2024-02-21 20:16:28 -0500

    Made it change to the password list page when you set the password.

--- neng-pass-gui/src/Setup.tsx
+++ neng-pass-gui/src/Setup.tsx
@@ -1,13 +1,17 @@
 import PasswordInput from "./components/PasswordInput"
 import Button from "./components/Button"
-import { createSignal } from "solid-js"
+import { createSignal, useContext } from "solid-js"
 import ErrorBox from "./components/ErrorBox"
 import { invoke } from "@tauri-apps/api"
+import { PageContext } from "./Index"
+import { Page } from "./common"
 
 export default function Setup() {
     const [getNewMasterKey, setNewMasterKey] = createSignal("")
     const [getConfirmMasterKey, setConfirmMasterKey] = createSignal("")
     const [getErrorBoxHeightClass, setErrorBoxHeightClass] = createSignal("max-h-0")
+
+    const pageContext = useContext(PageContext)
 
     return <div class="flex flex-col p-2 max-w-4xl m-10">
         <h1 class="text-4xl pb-4 select-none">Setup</h1>
@@ -24,12 +28,15 @@ export default function Setup() {
             setConfirmMasterKey(event.target.value.trim())
         }} />
 
-        <Button label="Continue" onClick={() => {
+        <Button label="Continue" onClick={async () => {
             if (getNewMasterKey() != getConfirmMasterKey()) {
                 setErrorBoxHeightClass("max-h-auto p-2 mb-6")
             } else {
-                invoke("set_new_master_key", { pNewMasterKey: getNewMasterKey() })
-                invoke("set_master_key", { pMasterKey: getNewMasterKey() })
+                await invoke("set_new_master_key", { pNewMasterKey: getNewMasterKey() })
+                await invoke("set_master_key", { pMasterKey: getNewMasterKey() })
+                if (pageContext != null) {
+                    pageContext.setPage(Page.Passwords)
+                }
             }
         }} />
     </div>
