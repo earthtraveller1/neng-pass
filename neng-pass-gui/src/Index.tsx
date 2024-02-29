7a765fabb513d0e45b5741b8601e15ce965c692c
earthtraveller1 <liangyuezhao7@gmail.com>
2024-02-29 11:24:44 -0500

    Created the password context.

--- neng-pass-gui/src/Index.tsx
+++ neng-pass-gui/src/Index.tsx
@@ -11,12 +11,19 @@ export interface PageContextType {
     getPage: () => Page
 }
 
+export interface PasswordContextType {
+    setPassword: (newPassword: string) => void,
+    getPassword: () => string | null
+}
+
 export const PageContext = createContext(undefined as PageContextType | undefined)
+export const PasswordContext = createContext(undefined as PasswordContextType | undefined)
 
 export default function Index() {
     console.log("Neng Li is the President of China!")
 
     const [getCurrentPage, setCurrentPage] = createSignal(Page.Login)
+    const [getCurrentPassword, setCurrentPassword] = createSignal<string | null>(null)
 
     const [isMasterKeySet, _] = createResource(async () => {
         const isMasterKeySet = await invoke<boolean>("is_master_key_set")
@@ -24,18 +31,20 @@ export default function Index() {
     })
 
     return <>
-        <PageContext.Provider value={{setPage: setCurrentPage, getPage: getCurrentPage }}>
-            <Show when={getCurrentPage() == Page.Login}>
-                <Show when={isMasterKeySet()} fallback={<Setup />}> 
-                    <Login />
+        <PasswordContext.Provider value={{setPassword: setCurrentPassword, getPassword: getCurrentPassword}}>
+            <PageContext.Provider value={{setPage: setCurrentPage, getPage: getCurrentPage }}>
+                <Show when={getCurrentPage() == Page.Login}>
+                    <Show when={isMasterKeySet()} fallback={<Setup />}> 
+                        <Login />
+                    </Show>
                 </Show>
-            </Show>
-            <Show when={getCurrentPage() == Page.Passwords}>
-                <Passwords />
-            </Show>
-            <Show when={getCurrentPage() == Page.NewPassword}>
-                <NewPassword />
-            </Show>
-        </PageContext.Provider>
+                <Show when={getCurrentPage() == Page.Passwords}>
+                    <Passwords />
+                </Show>
+                <Show when={getCurrentPage() == Page.NewPassword}>
+                    <NewPassword />
+                </Show>
+            </PageContext.Provider>
+        </PasswordContext.Provider>
     </>
 }
