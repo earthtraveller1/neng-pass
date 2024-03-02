4e9080d3423e24b9270f9907a4df18dfac95509d
Yue <liangyuezhao7@gmail.com>
2024-03-02 15:42:33 -0500

    Added buttons to go back and delete the password.

--- neng-pass-gui/src/Password.tsx
+++ neng-pass-gui/src/Password.tsx
@@ -1,5 +1,8 @@
 import { useContext } from "solid-js"
-import { PasswordContext } from "./Index"
+import { PageContext, PasswordContext } from "./Index"
+import Button, { ButtonStyle } from "./components/Button"
+import { Page } from "./common"
+import { invoke } from "@tauri-apps/api"
 
 export default function Password() {
     const passwordContext = useContext(PasswordContext)
@@ -7,12 +10,24 @@ export default function Password() {
         throw new Error("The password context must be provided!")
     }
 
+    const pageContext = useContext(PageContext)
+    if (pageContext == undefined) {
+        throw new Error("The page context was not set! What the actual fuck?")
+    }
+
     const password = passwordContext.getPassword()
     if (password == null) {
         throw new Error("There must be a password stored")
     }
 
-    return <div class="flex flex-col">
-        <h1 class="text-4xl py-4 mb-8 select-none">{password}</h1>
+    return <div class="flex flex-col p-4">
+        <h1 class="text-4xl py-4 mb-8 select-none text-center">{password}</h1>
+        <Button label="Delete" style={ButtonStyle.Red} onClick={async () => {
+            await invoke("delete_password", {pName: password})
+            pageContext.setPage(Page.Passwords)
+        }}/>
+        <Button label="Back" style={ButtonStyle.Gray} onClick={() => {
+            pageContext.setPage(Page.Passwords)
+        }}/>
     </div>
 }
