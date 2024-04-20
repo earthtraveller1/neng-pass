cf0d696751b8696ee11dc3422ffa8d5b51c442df
earthtraveller1 <liangyuezhao7@gmail.com>
2024-04-20 17:41:17 -0400

    Removed endpoint for creating passwords, added endpoint for generating passwords.

--- neng-pass-gui/src-tauri/src/main.rs
+++ neng-pass-gui/src-tauri/src/main.rs
@@ -85,11 +85,10 @@ fn get_password_list(p_state: tauri::State<'_, State>) -> Result<Vec<String>, St
 }
 
 #[tauri::command]
-fn create_password(
-    p_state: tauri::State<'_, State>,
-    p_name: &str,
-) -> Result<(), String> {
-    save_password(p_state, p_name, std::str::from_utf8(&neng_pass::generate_password()).unwrap())
+fn generate_password() -> Result<String, ()> {
+    std::str::from_utf8(&neng_pass::generate_password())
+        .map(|s| s.to_string())
+        .map_err(|_| ())
 }
 
 #[tauri::command]
@@ -103,7 +102,7 @@ fn save_password(
 
     let master_key = match &internal_state.master_key {
         Some(master_key) => master_key,
-        None => return Err("The master has not been set yet!".to_string())
+        None => return Err("The master has not been set yet!".to_string()),
     };
 
     match neng_pass::create_password(master_key.clone(), p_name, p_password, &sql_connection) {
@@ -140,7 +139,10 @@ async fn set_master_key(
 #[tauri::command]
 async fn get_password(p_name: &str, p_state: tauri::State<'_, State>) -> Result<String, String> {
     let internal_state = p_state.internal_state.lock().unwrap();
-    let master_key = internal_state.master_key.clone().ok_or("The master key has not been set!")?;
+    let master_key = internal_state
+        .master_key
+        .clone()
+        .ok_or("The master key has not been set!")?;
 
     let sql_connection = open_and_prepare_database(&p_state.static_state.data_dir)?;
     let password = neng_pass::get_password(master_key, p_name, &sql_connection)?;
@@ -149,10 +151,7 @@ async fn get_password(p_name: &str, p_state: tauri::State<'_, State>) -> Result<
 }
 
 #[tauri::command]
-async fn delete_password(
-    p_name: &str,
-    p_state: tauri::State<'_, State>
-) -> Result<(), String> {
+async fn delete_password(p_name: &str, p_state: tauri::State<'_, State>) -> Result<(), String> {
     let sql_connection = open_and_prepare_database(&p_state.static_state.data_dir)?;
     neng_pass::delete_password(p_name, &sql_connection)?;
 
@@ -192,14 +191,15 @@ fn main() {
     tauri::Builder::default()
         .manage(State::new())
         .invoke_handler(tauri::generate_handler![
-            get_password_list,
-            create_password,
-            is_master_key_correct,
-            set_master_key,
-            is_master_key_set,
-            set_new_master_key,
             delete_password,
+            generate_password,
             get_password,
+            get_password_list,
+            is_master_key_correct,
+            is_master_key_set,
+            save_password,
+            set_master_key,
+            set_new_master_key,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
