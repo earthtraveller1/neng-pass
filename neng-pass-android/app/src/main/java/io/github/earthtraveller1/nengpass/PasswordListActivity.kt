373b9d91b0fcdce2b29a70c0d7f96b064bc6cba7
Liangyue " Yue <liangyuezhao7@gmail.com>
2024-04-27 16:44:10 -0400

    Added an Element to represent a single password item.

--- neng-pass-android/app/src/main/java/io/github/earthtraveller1/nengpass/PasswordListActivity.kt
+++ neng-pass-android/app/src/main/java/io/github/earthtraveller1/nengpass/PasswordListActivity.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.*
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
 import io.github.earthtraveller1.nengpass.ui.theme.NengPassTheme
 
 class PasswordListActivity : ComponentActivity() {
@@ -52,6 +54,36 @@ class PasswordListActivity : ComponentActivity() {
         }
     }
 
+    @Composable
+    fun PasswordEntry(modifier: Modifier = Modifier, name: String) {
+        Button(
+            onClick = {
+
+            },
+            colors = buttonColors(
+                containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                contentColor = MaterialTheme.colorScheme.onSecondaryContainer
+            ),
+            shape = RoundedCornerShape(corner = CornerSize(10.dp)),
+            modifier = modifier
+                .padding(18.dp)
+                .fillMaxWidth(),
+        ) {
+            Row(
+                modifier = modifier.fillMaxWidth().padding(vertical = 5.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Lock,
+                    contentDescription = "A Lock",
+                    modifier = modifier.padding(horizontal = 10.dp)
+                )
+
+                Text(name)
+            }
+        }
+    }
+
     @Preview
     @Composable
     private fun MainContent(modifier: Modifier = Modifier) {
@@ -68,6 +100,9 @@ class PasswordListActivity : ComponentActivity() {
                         .fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
+                    Column(modifier = modifier) {
+                        PasswordEntry(modifier = modifier, name = "Neng Li")
+                    }
                 }
             }
         }
